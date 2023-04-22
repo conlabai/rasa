716188e794851132b01cd6147124f9522c71767d
Dimitri Tischenko <timidri@gmail.com>
2023-04-22 23:25:41 +0300

    remove obsolete config and files

--- r.sh
+++
@@ -1,6 +0,0 @@
-docker run -it --env-file .env \
--v  $(pwd)/endpoints.yml:/app/endpoints.yml \
--v $(pwd)/rasa-model/models:/app/models \
--v $(pwd)/rasa-server/credentials.yml:/app/credentials.yml \
--v $(pwd)/rasa-server/channels:/app/channels  \
---network dbt_default -p 0.0.0.0:8888:5005 eremeye/rasa-idl:dbt run --enable-api --debug
